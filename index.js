dedd009162a7dacd193669a81aa6a85a94e0a0a6
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-19 13:10:03 -0800

    remove unnecessary files and update aigle

--- index.js
+++ index.js
@@ -5,25 +5,5 @@ const Bluebird = require('bluebird');
 
 module.exports = {
   Aigle,
-  Bluebird,
-  resolveAigle,
-  resolveBluebird,
-  mapAigle,
-  mapBluebird
-}
-
-function resolveAigle(value) {
-  return new Aigle(resolve => setImmediate(() => resolve(value)));
-}
-
-function resolveBluebird(value) {
-  return new Bluebird(resolve => setImmediate(() => resolve(value)));
-}
-
-function mapAigle(array, iterator) {
-  return Aigle.map(array, iterator);
-}
-
-function mapBluebird(array, iterator) {
-  return Bluebird.map(array, iterator);
-}
+  Bluebird
+};
